b9f4fc1deebb90c11c09ef540d9c91d3a8b27863
苗泽鹏 <icecoffeemzp1993@gmail.com>
2020-09-18 14:04:57 +0800

    1. ShadowView.kt

---
+++ app/src/view_paint/java/com/sample/view/weights/BasicPaintView.kt
@@ -0,0 +1,124 @@
+package com.sample.view.weights
+
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
+import android.graphics.Path
+import android.util.AttributeSet
+import android.view.View
+import com.sample.common.utils.dp2px
+
+/**
+ * @date: 2020/9/12
+<p>
+ * @author: ice_coffee
+<p>
+ * @remark:
+ */
+class BasicPaintView : View {
+
+    lateinit var mPaint: Paint
+
+    constructor(context: Context):super(context){ initView() }
+    constructor(context: Context, attributes: AttributeSet?):super(context, attributes){ initView() }
+    constructor(context: Context, attributes: AttributeSet?, defStyleAttr: Int):super(context, attributes, defStyleAttr){ initView() }
+
+    private fun initView() {
+
+        mPaint = Paint()
+
+        mPaint.isAntiAlias = true //抗锯齿功能
+        mPaint.color = Color.RED //设置画笔颜色
+        mPaint.strokeWidth = 5f //设置画笔宽度
+        mPaint.textSize = dp2px(32f).toFloat()
+    }
+
+    override fun onDraw(canvas: Canvas?) {
+        super.onDraw(canvas)
+
+//        paintStyle(canvas!!)
+//        paintCap(canvas!!)
+        paintJoin(canvas!!)
+    }
+
+    /**
+     * Paint.Style
+     * 填充模式
+     * Paint.Style.FILL             : 填充内部
+     * Paint.Style.STROKE           : 填充描边
+     * Paint.Style.FILL_AND_STROKE  : 填充内部和描边
+     */
+    private fun paintStyle(canvas: Canvas) {
+
+        //default
+        canvas.drawCircle(100f, 100f, 100f, mPaint)
+
+        //Paint.Style.FILL
+        canvas.translate(250f, 0f)
+        mPaint.style = Paint.Style.FILL
+        canvas.drawCircle(100f, 100f, 100f, mPaint)
+
+        //Paint.Style.STROKE
+        canvas.translate(250f, 0f)
+        mPaint.style = Paint.Style.STROKE
+        canvas.drawCircle(100f, 100f, 100f, mPaint)
+
+        //Paint.Style.FILL_AND_STROKE
+        canvas.translate(250f, 0f)
+        mPaint.style = Paint.Style.FILL_AND_STROKE
+        canvas.drawCircle(100f, 100f, 100f, mPaint)
+    }
+
+    /**
+     * Paint.Cap
+     * 线段开始和结束样式
+     * Paint.Cap.BUTT             : 默认不添加样式
+     * Paint.Cap.ROUND            : 半圆样式
+     * Paint.Cap.SQUARE           : 长方形样式
+     */
+    private fun paintCap(canvas: Canvas) {
+
+        mPaint.strokeWidth = dp2px(30f).toFloat()
+
+        mPaint.strokeCap = Paint.Cap.BUTT
+        canvas.drawLine(50f, 10f, 200f, 10f, mPaint)
+
+        canvas.translate(0f, 200f)
+        mPaint.strokeCap = Paint.Cap.ROUND
+        canvas.drawLine(50f, 10f, 200f, 10f, mPaint)
+
+        canvas.translate(0f, 200f)
+        mPaint.strokeCap = Paint.Cap.SQUARE
+        canvas.drawLine(50f, 10f, 200f, 10f, mPaint)
+    }
+
+    /**
+     * Paint.Join (测试无效)
+     * 线段连接方式(拐角类型)
+     * Paint.Join.MITER         : 尖角 (默认模式)
+     * Paint.Join.BEVEL	        : 平角
+     * Paint.Join.ROUND	        : 圆角
+     */
+    private fun paintJoin(canvas: Canvas) {
+
+        val path = Path()
+        path.moveTo(100f, 100f)
+        path.lineTo(450f, 100f)
+        path.lineTo(100f, 300f)
+        mPaint.strokeJoin = Paint.Join.MITER
+        canvas.drawPath(path, mPaint)
+
+        path.moveTo(100f, 400f)
+        path.lineTo(450f, 400f)
+        path.lineTo(100f, 600f)
+        mPaint.strokeJoin = Paint.Join.BEVEL
+        canvas.drawPath(path, mPaint)
+
+        path.moveTo(100f, 700f)
+        path.lineTo(450f, 700f)
+        path.lineTo(100f, 900f)
+        mPaint.strokeJoin = Paint.Join.ROUND
+        canvas.drawPath(path, mPaint)
+    }
+}
--- app/src/view_paint/java/com/sample/view/weights/ShadowView.kt
+++ app/src/view_paint/java/com/sample/view/weights/ShadowView.kt
@@ -6,15 +6,14 @@ import android.graphics.Color
 import android.graphics.Paint
 import android.util.AttributeSet
 import android.view.View
+import com.sample.common.utils.dp2px
 
 /**
- * @date: 2020/9/12
-<p>
- * @author: ice_coffee
-<p>
- * @remark:
+ *  @author mzp
+ *  date : 2020/9/18
+ *  desc :
  */
-class PaintView : View {
+class ShadowView: View {
 
     lateinit var mPaint: Paint
 
@@ -23,47 +22,19 @@ class PaintView : View {
     constructor(context: Context, attributes: AttributeSet?, defStyleAttr: Int):super(context, attributes, defStyleAttr){ initView() }
 
     private fun initView() {
+
         mPaint = Paint()
 
         mPaint.isAntiAlias = true //抗锯齿功能
         mPaint.color = Color.RED //设置画笔颜色
         mPaint.strokeWidth = 5f //设置画笔宽度
-        mPaint.textSize = 100f //设置字体大小
+        mPaint.textSize = dp2px(32f).toFloat()
     }
 
     override fun onDraw(canvas: Canvas?) {
         super.onDraw(canvas)
 
-//        paintStyle(canvas)
-        setShadowLayer(canvas)
-    }
-
-    /**
-     * Paint.Style
-     * 填充模式
-     * Paint.Style.FILL             : 填充内部
-     * Paint.Style.STROKE           : 填充描边
-     * Paint.Style.FILL_AND_STROKE  : 填充内部和描边
-     */
-    private fun paintStyle(canvas: Canvas?) {
-
-        //default
-        canvas?.drawCircle(100f, 100f, 100f, mPaint)
-
-        //Paint.Style.FILL
-        canvas?.translate(250f, 0f)
-        mPaint.style = Paint.Style.FILL
-        canvas?.drawCircle(100f, 100f, 100f, mPaint)
-
-        //Paint.Style.STROKE
-        canvas?.translate(250f, 0f)
-        mPaint.style = Paint.Style.STROKE
-        canvas?.drawCircle(100f, 100f, 100f, mPaint)
-
-        //Paint.Style.FILL_AND_STROKE
-        canvas?.translate(250f, 0f)
-        mPaint.style = Paint.Style.FILL_AND_STROKE
-        canvas?.drawCircle(100f, 100f, 100f, mPaint)
+        drawTextShadow(canvas!!)
     }
 
     /**
@@ -75,9 +46,9 @@ class PaintView : View {
      * @param color 阴影颜色
      * 可用于创建文字下面的模糊阴影。对其他绘图操作的支持仅限于软件渲染管道。
      */
-    private fun setShadowLayer(canvas: Canvas?) {
+    private fun drawTextShadow(canvas: Canvas) {
 
         mPaint.setShadowLayer(10f, 5f, 5f, Color.GREEN) //设置阴影
-        canvas?.drawText("Paint_Style", 110f, 110f, mPaint)
+        canvas.drawText("Paint_Style", 110f, 110f, mPaint)
     }
 }
