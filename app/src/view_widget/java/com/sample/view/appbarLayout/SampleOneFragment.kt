de716d35cef7c342110d33b0cacd2dad03f4c584
苗泽鹏 <icecoffeemzp1993@gmail.com>
2020-09-30 12:03:07 +0800

    1. SampleTwoBehavior.kt

--- app/src/view_widget/java/com/sample/view/appbarLayout/SampleOneFragment.kt
+++ app/src/view_widget/java/com/sample/view/appbarLayout/SampleOneFragment.kt
@@ -13,6 +13,7 @@ import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.RecyclerView.Adapter
+import com.sample.common.utils.LogUtils
 import com.sample.view.R
 import kotlinx.android.synthetic.main.fragment_appbar_one.*
 import java.util.*
@@ -35,31 +36,6 @@ class SampleOneFragment: Fragment() {
         myList.adapter = MyAdapter(context!!)
     }
 
-    class MyAdapter(context: Context) : Adapter<MyViewHolder>() {
-
-        private val colorArray = arrayOf("#FADB14", "#FFD6E7", "#D9D9D9", "#BAE7FF", "#D3F261", "#BAC5F3", "#FFEADB", "#FF847C", "#D9F7BE", "#FFEECC")
-        private val random = Random()
-        private val layoutInflater: LayoutInflater = LayoutInflater.from(context)
-
-        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
-            return MyViewHolder(layoutInflater.inflate(R.layout.view_item, parent, false))
-        }
-
-        override fun getItemCount(): Int {
-            return 15
-        }
-
-        override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
-            holder.tvItem.text = "item$position"
-            holder.tvItem.setBackgroundColor(Color.parseColor(colorArray[random.nextInt(colorArray.size)]))
-        }
-    }
-
-    class MyViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-
-        val tvItem: TextView = view.findViewById(R.id.tvItem)
-    }
-
     class SampleOneBehavior : CoordinatorLayout.Behavior<View?> {
         // 列表顶部和title底部重合时，列表的滑动距离。
         private var deltaY = 0f
@@ -80,8 +56,40 @@ class SampleOneFragment: Fragment() {
             dy = if (dy < 0) 0f else dy
             val y = -(dy / deltaY) * child.height
             child.translationY = y
+            LogUtils.e(y)
+
+            /**
+             * 透明度变化
+             * val y = 1 - (dy / deltaY)
+             * child.alpha = y
+             */
 
             return true
         }
+    }
+}
+
+class MyAdapter(context: Context) : Adapter<MyAdapter.MyViewHolder>() {
+
+    private val colorArray = arrayOf("#FADB14", "#FFD6E7", "#D9D9D9", "#BAE7FF", "#D3F261", "#BAC5F3", "#FFEADB", "#FF847C", "#D9F7BE", "#FFEECC")
+    private val random = Random()
+    private val layoutInflater: LayoutInflater = LayoutInflater.from(context)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyAdapter.MyViewHolder {
+        return MyAdapter.MyViewHolder(layoutInflater.inflate(R.layout.view_item, parent, false))
+    }
+
+    override fun getItemCount(): Int {
+        return 15
+    }
+
+    override fun onBindViewHolder(holder: MyAdapter.MyViewHolder, position: Int) {
+        holder.tvItem.text = "item$position"
+        holder.tvItem.setBackgroundColor(Color.parseColor(colorArray[random.nextInt(colorArray.size)]))
+    }
+
+    class MyViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+
+        val tvItem: TextView = view.findViewById(R.id.tvItem)
     }
 }
--- app/src/view_widget/java/com/sample/view/appbarLayout/SampleTwoFragment.kt
+++ app/src/view_widget/java/com/sample/view/appbarLayout/SampleTwoFragment.kt
@@ -1,11 +1,17 @@
 package com.sample.view.appbarLayout
 
+import android.content.Context
 import android.os.Bundle
+import android.util.AttributeSet
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.coordinatorlayout.widget.CoordinatorLayout
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.sample.view.R
+import kotlinx.android.synthetic.main.fragment_appbar_one.*
 
 /**
  *  @author mzp
@@ -20,5 +26,16 @@ class SampleTwoFragment: Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        myList.layoutManager = LinearLayoutManager(context)
+        myList.adapter = MyAdapter(context!!)
+    }
+
+    class RecyclerViewBehavior : CoordinatorLayout.Behavior<RecyclerView> {
+
+        constructor():super(){}
+        constructor(context: Context, attributeSet: AttributeSet):super(context, attributeSet){}
+
+
     }
 }
