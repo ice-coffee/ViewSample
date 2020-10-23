29ba8359ef85bf66fff05bc1ec96b3483a3b9f2e
苗泽鹏 <icecoffeemzp1993@gmail.com>
2020-10-23 19:19:15 +0800

    1. GetFillPathView.kt

---
+++ app/src/view_paint/java/com/sample/view/weights/GetFillPathView.kt
@@ -0,0 +1,43 @@
+package com.sample.view.weights
+
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.graphics.Path
+import android.graphics.RectF
+import android.util.AttributeSet
+import android.view.View
+
+/**
+ *  @author mzp
+ *  date : 2020/10/23
+ *  desc :
+ */
+class GetFillPathView(context: Context?, attrs: AttributeSet?) : View(context, attrs) {
+
+    override fun onDraw(canvas: Canvas?) {
+        super.onDraw(canvas)
+
+        val arcPath = Path()
+        val borderPath = Path()
+
+        arcPath.addArc(RectF(100f, 100f, 500f, 500f), 30f, 300f)
+
+        val paint = Paint()
+        paint.style = Paint.Style.STROKE
+        paint.strokeCap = Paint.Cap.ROUND
+        paint.strokeWidth = 100f
+
+        canvas!!.drawPath(arcPath, paint)
+
+        canvas.translate(0f, 500f)
+
+        //获取
+        paint.getFillPath(arcPath, borderPath)
+
+        paint.style = Paint.Style.STROKE
+        paint.strokeWidth = 2f
+        paint.isAntiAlias = true
+        canvas.drawPath(borderPath, paint)
+    }
+}
