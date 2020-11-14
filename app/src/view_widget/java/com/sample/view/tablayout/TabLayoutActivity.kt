4466a82328368b0748ee9916f74196db01891b0e
miaozepeng <2677344716@qq.com>
2020-11-14 12:47:44 +0800

    TabLayout

--- app/src/view_widget/java/com/sample/view/tablayout/TabLayoutActivity.kt
+++ app/src/view_widget/java/com/sample/view/tablayout/TabLayoutActivity.kt
@@ -1,6 +1,9 @@
 package com.sample.view.tablayout
 
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import com.sample.view.MyFragmentPagerAdapter
@@ -15,6 +18,7 @@ import kotlinx.android.synthetic.main.activity_tab_layout.*
 class TabLayoutActivity: AppCompatActivity() {
 
     private val pageTitles = arrayListOf("首页", "消息", "我")
+    private val tabIcons = arrayListOf(R.drawable.selector_ic_home, R.drawable.selector_ic_infor, R.drawable.selector_ic_mine)
     private val pagerFragmengs = arrayListOf<Fragment>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -27,9 +31,14 @@ class TabLayoutActivity: AppCompatActivity() {
         viewpager.adapter = MyFragmentPagerAdapter(supportFragmentManager, pageTitles, pagerFragmengs)
         tabLayout.setupWithViewPager(viewpager)
 
-        tabLayout.getTabAt(0)?.setIcon(R.mipmap.ic_home)
-        tabLayout.getTabAt(1)?.setIcon(R.mipmap.ic_infor)
-        tabLayout.getTabAt(2)?.setIcon(R.mipmap.ic_mine)
+        for (i in 0 until pageTitles.size) {
+            val view = LayoutInflater.from(this).inflate(R.layout.item_tab_layout, null)
+            val ivTabIcon = view.findViewById<ImageView>(R.id.ivTabIcon)
+            val tvTabContent = view.findViewById<TextView>(R.id.tvTabContent)
+            ivTabIcon.setImageResource(tabIcons[i])
+            tvTabContent.text = pageTitles[i]
+            tabLayout.getTabAt(i)?.customView = view
+        }
     }
 
     private fun initFragmentList() {
