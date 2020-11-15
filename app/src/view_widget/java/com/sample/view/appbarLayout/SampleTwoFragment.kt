bc3b3ea2b952270876208e53574127ec817178fe
miaozepeng <2677344716@qq.com>
2020-11-15 16:10:33 +0800

    appbarlayout

--- app/src/view_widget/java/com/sample/view/appbarLayout/SampleTwoFragment.kt
+++ app/src/view_widget/java/com/sample/view/appbarLayout/SampleTwoFragment.kt
@@ -4,16 +4,14 @@ import android.content.Context
 import android.os.Bundle
 import android.util.AttributeSet
 import android.view.LayoutInflater
-import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import android.widget.ImageView
 import androidx.coordinatorlayout.widget.CoordinatorLayout
 import androidx.core.view.ViewCompat
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.sample.common.utils.LogUtils
 import com.sample.view.R
 import kotlinx.android.synthetic.main.fragment_appbar_one.*
 
@@ -37,64 +35,40 @@ class SampleTwoFragment: Fragment() {
     }
 }
 
-class SampleHeaderBehavior : CoordinatorLayout.Behavior<TextView> {
-    // 界面整体向上滑动，达到列表可滑动的临界点
-    private var upReach = false
-
-    // 列表向上滑动后，再向下滑动，达到界面整体可滑动的临界点
-    private var downReach = false
-
-    // 列表上一个全部可见的item位置
-    private var lastPosition = -1
+class SampleHeaderBehavior : CoordinatorLayout.Behavior<ImageView> {
 
     constructor() {}
     constructor(context: Context?, attrs: AttributeSet?) : super(context, attrs) {}
 
-    override fun onInterceptTouchEvent(parent: CoordinatorLayout, child: TextView, ev: MotionEvent): Boolean {
-        when (ev.action) {
-            MotionEvent.ACTION_DOWN -> {
-                downReach = false
-                upReach = false
-            }
-        }
-        return super.onInterceptTouchEvent(parent, child, ev)
-    }
-
-    override fun onStartNestedScroll(coordinatorLayout: CoordinatorLayout, child: TextView, directTargetChild: View, target: View, axes: Int, type: Int): Boolean {
+    override fun onStartNestedScroll(coordinatorLayout: CoordinatorLayout, child: ImageView, directTargetChild: View, target: View, axes: Int, type: Int): Boolean {
         return axes and ViewCompat.SCROLL_AXIS_VERTICAL != 0
     }
 
-    override fun onNestedPreScroll(coordinatorLayout: CoordinatorLayout, child: TextView, target: View, dx: Int, dy: Int, consumed: IntArray, type: Int) {
+    override fun onNestedPreScroll(coordinatorLayout: CoordinatorLayout, child: ImageView, target: View, dx: Int, dy: Int, consumed: IntArray, type: Int) {
         super.onNestedPreScroll(coordinatorLayout, child, target, dx, dy, consumed, type)
         if (target is RecyclerView) {
-            // 列表第一个全部可见Item的位置
-            val pos = (target.layoutManager as LinearLayoutManager?)!!.findFirstCompletelyVisibleItemPosition()
-            if (pos == 0 && pos < lastPosition) {
-                downReach = true
-            }
-            // 整体可以滑动，否则RecyclerView消费滑动事件
-            if (canScroll(child, dy.toFloat()) && pos == 0) {
-                LogUtils.e("${child.translationY}, $dy")
-                var finalY = child.translationY - dy
-                if (finalY < -child.height) {
-                    finalY = -child.height.toFloat()
-                    upReach = true
-                } else if (finalY > 0) {
-                    finalY = 0f
-                }
-                child.translationY = finalY
-                // 让CoordinatorLayout消费滑动事件
-                consumed[1] = dy
-            }
-            lastPosition = pos
-        }
-    }
+            val layoutManager = target.layoutManager
+            if (layoutManager is LinearLayoutManager) {
+                val pos = layoutManager.findFirstCompletelyVisibleItemPosition()
 
-    private fun canScroll(child: View, scrollY: Float): Boolean {
-        if (scrollY > 0 && child.translationY == -child.height.toFloat() && !upReach) {
-            return false
+                if (pos == 0) {
+                    var customY = 0f
+                    var finalY = child.translationY - dy
+                    if (finalY > -child.height && finalY < 0) {
+                        customY = dy.toFloat()
+                    } else if (finalY < -child.height) {
+                        customY = finalY - (-child.height)
+                        finalY = -child.height.toFloat()
+                    } else if (finalY > 0) {
+                        customY = finalY - 0
+                        finalY = 0f
+                    }
+
+                    consumed[1] = customY.toInt()
+                    child.translationY = finalY
+                }
+            }
         }
-        return !downReach
     }
 }
 
@@ -103,7 +77,7 @@ class RecyclerViewBehavior : CoordinatorLayout.Behavior<RecyclerView> {
     constructor(context: Context?, attrs: AttributeSet?) : super(context, attrs) {}
 
     override fun layoutDependsOn(parent: CoordinatorLayout, child: RecyclerView, dependency: View): Boolean {
-        return dependency is TextView
+        return dependency is ImageView
     }
 
     override fun onDependentViewChanged(parent: CoordinatorLayout, child: RecyclerView, dependency: View): Boolean {
