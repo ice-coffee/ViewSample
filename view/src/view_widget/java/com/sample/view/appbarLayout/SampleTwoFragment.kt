5ca5760ba855130b6c34dedfd9115a00ec7690a7
miaozepeng <2677344716@qq.com>
2021-03-14 13:14:53 +0800

    1. Appbar 更新

--- view/src/view_widget/java/com/sample/view/appbarLayout/SampleTwoFragment.kt
+++ view/src/view_widget/java/com/sample/view/appbarLayout/SampleTwoFragment.kt
@@ -3,15 +3,19 @@ package com.sample.view.appbarLayout
 import android.content.Context
 import android.os.Bundle
 import android.util.AttributeSet
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.TextView
 import androidx.coordinatorlayout.widget.CoordinatorLayout
 import androidx.core.view.ViewCompat
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.sample.common.utils.dp2px
 import com.sample.view.R
 import kotlinx.android.synthetic.main.fragment_appbar_one.*
 
@@ -21,7 +25,7 @@ import kotlinx.android.synthetic.main.fragment_appbar_one.*
  *  date : 2020/9/29
  *  desc :
  */
-class SampleTwoFragment: Fragment() {
+class SampleTwoFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_appbar_two, container, false)
@@ -35,6 +39,70 @@ class SampleTwoFragment: Fragment() {
     }
 }
 
+class SampleLinearBehavior : CoordinatorLayout.Behavior<LinearLayout> {
+
+    private val maxY = dp2px(200).toFloat()
+    private val minY = dp2px(60).toFloat()
+
+    constructor() {}
+    constructor(context: Context?, attrs: AttributeSet?) : super(context, attrs) {}
+
+    override fun onStartNestedScroll(coordinatorLayout: CoordinatorLayout, child: LinearLayout, directTargetChild: View, target: View, axes: Int, type: Int): Boolean {
+        return axes == ViewCompat.SCROLL_AXIS_VERTICAL
+    }
+
+    override fun onNestedPreScroll(coordinatorLayout: CoordinatorLayout, child: LinearLayout, target: View, dx: Int, dy: Int, consumed: IntArray, type: Int) {
+        if (target is RecyclerView) {
+            val layoutManager = target.layoutManager
+            if (layoutManager is LinearLayoutManager) {
+                val pos = layoutManager.findFirstCompletelyVisibleItemPosition()
+                if (pos == 0) {
+                    var customY = 0f
+                    var finalY = child.translationY - dy
+                    if (finalY > minY) {
+                        if (finalY > maxY) {
+                            finalY = maxY
+                        }
+                        customY = dy.toFloat()
+                    } else {
+                        if (child.translationY > minY) {
+                            customY = child.translationY - minY
+                        } else {
+                            customY = 0f
+                        }
+                        finalY = minY
+                    }
+                    consumed[1] = customY.toInt()
+                    child.translationY = finalY
+                }
+            }
+        }
+    }
+
+    override fun onLayoutChild(parent: CoordinatorLayout, child: LinearLayout, layoutDirection: Int): Boolean {
+        child.translationY = maxY
+        return false
+    }
+}
+
+class SampleTextViewBehavior : CoordinatorLayout.Behavior<TextView> {
+
+    private val minY = dp2px(60).toFloat()
+
+    constructor() {}
+    constructor(context: Context?, attrs: AttributeSet?) : super(context, attrs) {}
+
+    override fun layoutDependsOn(parent: CoordinatorLayout, child: TextView, dependency: View): Boolean {
+        return dependency is LinearLayout
+    }
+
+    override fun onDependentViewChanged(parent: CoordinatorLayout, child: TextView, dependency: View): Boolean {
+
+        child.visibility = if (dependency.translationY == minY) View.VISIBLE else View.INVISIBLE
+        return true
+    }
+}
+
 class SampleHeaderBehavior : CoordinatorLayout.Behavior<ImageView> {
 
     constructor() {}
@@ -43,6 +111,11 @@ class SampleHeaderBehavior : CoordinatorLayout.Behavior<ImageView> {
     override fun onStartNestedScroll(coordinatorLayout: CoordinatorLayout, child: ImageView, directTargetChild: View, target: View, axes: Int, type: Int): Boolean {
         return axes == ViewCompat.SCROLL_AXIS_VERTICAL
     }
+
+//    override fun onLayoutChild(parent: CoordinatorLayout, child: ImageView, layoutDirection: Int): Boolean {
+//        Log.e("behavior", "onLayoutChild")
+//        return super.onLayoutChild(parent, child, layoutDirection)
+//    }
 
     override fun onNestedPreScroll(coordinatorLayout: CoordinatorLayout, child: ImageView, target: View, dx: Int, dy: Int, consumed: IntArray, type: Int) {
         super.onNestedPreScroll(coordinatorLayout, child, target, dx, dy, consumed, type)
