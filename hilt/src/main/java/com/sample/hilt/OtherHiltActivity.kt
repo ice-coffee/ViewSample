2112a47321fe5902224057c275673cd8e63b1b97
苗泽鹏 <icecoffeemzp1993@gmail.com>
2021-04-29 11:29:50 +0800

    1. 名称调整

--- hilt/src/main/java/com/sample/hilt/OtherHiltActivity.kt
+++ hilt/src/main/java/com/sample/hilt/OtherHiltActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.sample.hilt.bean.UserBean
+import com.sample.hilt.bean.WaterBean
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
 
@@ -18,8 +19,14 @@ class OtherHiltActivity: AppCompatActivity() {
     @Inject
     lateinit var userBean: UserBean
 
+    @Inject
+    lateinit var waterBean: WaterBean
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_other_hilt)
+
+        Log.e("hilt", userBean.toString())
+        Log.e("hilt", waterBean.toString())
     }
 }
